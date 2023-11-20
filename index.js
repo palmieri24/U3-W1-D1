6d8a015d9f40634876f9704efa1ef62f3b7c8e40
Alessia Palmieri <146003639+palmieri24@users.noreply.github.com>
2023-11-20 16:50:44 +0100

    first commit without ts

---
+++ index.js
@@ -0,0 +1,43 @@
+let Player1 = document.getElementById("Player1");
+let Player2 = document.getElementById("Player2");
+let formBtn = document.getElementById("formBtn");
+
+let resultPlayer1 = document.createElement("p");
+let resultPlayer2 = document.createElement("p");
+let RandomNumber = document.createElement("p");
+let checkWinner = document.createElement("p");
+
+document.body.appendChild(resultPlayer1);
+document.body.appendChild(resultPlayer2);
+document.body.appendChild(RandomNumber);
+document.body.append(checkWinner);
+
+console.log(Player1);
+console.log(Player2);
+
+let playRandomNumberGame = function (e) {
+  e.preventDefault();
+  checkWinner.innerText = "";
+  let n1 = parseInt(Player1.value);
+  let n2 = parseInt(Player2.value);
+  let random = Math.floor(Math.random() * 10);
+  resultPlayer1.innerText = "Giocatore 1: " + n1;
+  resultPlayer2.innerText = "Giocatore 2: " + n2;
+  RandomNumber.innerText = "Numero estratto: " + random;
+  if (n1 === random) {
+    checkWinner.innerText = "Giocatore 1 ha indovinato";
+  }
+  if (n2 === random) {
+    checkWinner.innerText = "Giocatore 2 ha indovinato";
+  }
+  if (n1 !== random && n2 !== random) {
+    if (random - n1 >= random - n2) {
+      checkWinner.innerText =
+        "Nessuno ha vinto ma il Player2 è andato più vicino";
+    } else {
+      checkWinner.innerText =
+        "Nessuno ha vinto ma il Player1 è andato più vicino";
+    }
+  }
+};
+formBtn.addEventListener("click", playRandomNumberGame);
